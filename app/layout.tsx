f1df27666d6ca5e28f3039f4783774fd58970355
Jasmine <105261487+codewithjazzy@users.noreply.github.com>
2025-03-07 19:00:18 -0800

    clerk tester

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,22 @@
+import { Metadata } from 'next';
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Dashboard',
+    default: 'Dashboard',
+  },
+  description: 'Next.js Dashboard, built with App Router.',
+};
 
 export default function RootLayout({
   children,
@@ -7,8 +24,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
